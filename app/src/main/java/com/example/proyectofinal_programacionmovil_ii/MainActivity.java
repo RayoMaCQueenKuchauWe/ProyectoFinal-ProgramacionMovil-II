ca660dc1cb389ad249c4007e1663199011dc92b7
andy <andy635.cs@gmailcom>
2020-11-22 16:22:00 -0400

    Marking the main menu navigable with the options of requirement and icon, logo, activity. Joseph :)

--- app/src/main/java/com/example/proyectofinal_programacionmovil_ii/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal_programacionmovil_ii/MainActivity.java
@@ -1,18 +1,88 @@
 package com.example.proyectofinal_programacionmovil_ii;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //Variable
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Hello work
+        /*------------ Hooks ------------*/
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        /*------------ Tool Bar ------------*/
+        setSupportActionBar(toolbar);
+
+        /*------------ Navigation Drawer Menu ------------*/
+        //Hide or show items
+        Menu menu = navigationView.getMenu();
+        menu.findItem(R.id.nav_profile).setVisible(false);
+        menu.findItem(R.id.nav_logout).setVisible(false);
+        //Setting
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        //State the navigation
+        navigationView.setCheckedItem(R.id.nav_home);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_about:
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_add:
+                Toast.makeText(this, "New form", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_close:
+                Toast.makeText(this, "Close", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_login:
+                Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
